f100023563ac7ad8744ea1f998788a43ce437fba
Ivan S Glazunov <ivansglazunov@gmail.com>
2021-02-16 20:21:58 +0000

    assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   basePath: '/store',
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/deepcase/deepcase.github.io/gh-pages/' : '',
 };
