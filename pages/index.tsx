3da6f2ec45ddd95c99b13fa0922a6c31b3d81fad
Ivan S Glazunov <ivansglazunov@gmail.com>
2021-02-16 17:59:55 +0000

    0.0.28

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,89 +8,116 @@ import { CapacitorStoreProvider, useCapacitorStore } from '@deepcase/store/capac
 
 import { IUseStore } from '@deepcase/store/store';
 
-export function ContentQuery() {
+import pckg from '../node_modules/@deepcase/store/package.json';
+
+const counters = {
+  page: 0,
+  query: 0,
+  queryListener: 0,
+  cookies: 0,
+  cookiesListener: 0,
+  local: 0,
+  localListener: 0,
+  capacitor: 0,
+  capacitorListener: 0,
+  token: 0,
+  contextual1: 0,
+  contextual1Listener: 0,
+  contextual2: 0,
+  contextual2Listener: 0,
+};
+export const ContentQuery = React.memo<any>(function ContentQuery() {
   const [value, setValue, unsetValue] = useQueryStore('demo', 5);
   return <>
+    <div><i>rerendered counter: {counters.query++}</i></div>
     <div>const [value, setValue, unsetValue] = useQueryStore('demo', 5);</div>
     <div>value == {value}</div>
     <button onClick={() => setValue(value - 1)}>-</button>
     <button onClick={() => setValue(value + 1)}>+</button>
     <button onClick={() => unsetValue()}>x</button>
   </>;
-}
+});
 
-export function ContentQueryListener() {
+export const ContentQueryListener = React.memo<any>(function ContentQueryListener() {
   const [value, setValue, unsetValue] = useQueryStore('demo', 5);
   return <>
+    <div><i>rerendered counter: {counters.queryListener++}</i></div>
     <div>const [value] = useQueryStore('demo', 5);</div>
     <div>value == {value}</div>
   </>;
-}
+});
 
-export function ContentCookies() {
+export const ContentCookies = React.memo<any>(function ContentCookies() {
   const [value, setValue, unsetValue] = useCookiesStore('demo', 5);
   return <>
+    <div><i>rerendered counter: {counters.cookies++}</i></div>
     <div>const [value, setValue, unsetValue] = useCookiesStore('demo', 5);</div>
     <div>value == {value}</div>
     <button onClick={() => setValue(value - 1)}>-</button>
     <button onClick={() => setValue(value + 1)}>+</button>
     <button onClick={() => unsetValue()}>x</button>
   </>;
-}
+});
 
-export function ContentCookiesListener() {
+export const ContentCookiesListener = React.memo<any>(function ContentCookiesListener() {
   const [value, setValue, unsetValue] = useCookiesStore('demo', 5);
   return <>
+    <div><i>rerendered counter: {counters.cookiesListener++}</i></div>
     <div>const [value] = useCookiesStore('demo', 5);</div>
     <div>value == {value}</div>
   </>;
-}
+});
 
-export function ContentLocal() {
+export const ContentLocal = React.memo<any>(function ContentLocal() {
   const [value, setValue, unsetValue] = useLocalStore('demo', 5);
   return <>
+    <div><i>rerendered counter: {counters.local++}</i></div>
     <div>const [value, setValue, unsetValue] = useLocalStore('demo', 5);</div>
     <div>value == {value}</div>
     <button onClick={() => setValue(value - 1)}>-</button>
     <button onClick={() => setValue(value + 1)}>+</button>
     <button onClick={() => unsetValue()}>x</button>
   </>;
-}
+});
 
-export function ContentLocalListener() {
+export const ContentLocalListener = React.memo<any>(function ContentLocalListener() {
   const [value, setValue, unsetValue] = useLocalStore('demo', 5);
   return <>
+    <div><i>rerendered counter: {counters.localListener++}</i></div>
     <div>const [value] = useLocalStore('demo', 5);</div>
     <div>value == {value}</div>
   </>;
-}
+});
 
-export function ContentCapacitor() {
+export const ContentCapacitor = React.memo<any>(function ContentCapacitor() {
   const [value, setValue, unsetValue] = useCapacitorStore('demo', 5);
   return <>
+    <div><i>rerendered counter: {counters.capacitor++}</i></div>
     <div>const [value, setValue, unsetValue] = useCapacitorStore('demo', 5);</div>
     <div>value == {value}</div>
     <button onClick={() => setValue(value - 1)}>-</button>
     <button onClick={() => setValue(value + 1)}>+</button>
     <button onClick={() => unsetValue()}>x</button>
   </>;
-}
+});
 
-export function ContentCapacitorListener() {
+export const ContentCapacitorListener = React.memo<any>(function ContentCapacitorListener() {
   const [value, setValue, unsetValue] = useCapacitorStore('demo', 5);
   return <>
+    <div><i>rerendered counter: {counters.capacitorListener++}</i></div>
     <div>const [value] = useCapacitorStore('demo', 5);</div>
     <div>value == {value}</div>
   </>;
-}
+});
 
-export function useToken() {
+export const useToken = function useToken() {
   return useCookiesStore('my-token-key', 0);
 }
-export function ContentToken() {
+export const ContentToken = React.memo<any>(function ContentToken() {
   const [token, setToken, unsetToken] = useToken();
   return <>
     <b>prepare storess</b>
+    <div><i>rerendered counter: {counters.token++}</i></div>
     <div>{`export function useToken() { return useCookiesStore('my-token-key', 0); }`}</div>
     <div>{`const [token, setToken] = useToken();`}</div>
     <div>token == {token}</div>
@@ -98,66 +125,75 @@ export function ContentToken() {
     <button onClick={() => setToken(token + 1)}>+</button>
     <button onClick={() => unsetToken()}>x</button>
   </>;
-}
+});
 
 
 export interface IOptions <T>{
   (defaultValue: T): [T, (value: T) => any, () => any];
 }
 export const OptionsContext = React.createContext<IOptions<any>>((defaultValue: any) => [null, () => null, () => {}]);
-export function OptionsProvider({ key, children }: { key: string; children: any }) {
+export const OptionsProvider = React.memo<any>(function OptionsProvider({ key, children }: { key: string; children: any }) {
   const useStore = React.useMemo(() => {
     return function useOptions(defualtValue) {
       return useQueryStore(key, defualtValue);
     };
   }, []);
   return <OptionsContext.Provider value={useStore}>{children}</OptionsContext.Provider>
-}
-export function useOptions(defualtValue: any) {
+});
+export const useOptions = function useOptions(defualtValue: any) {
   return React.useContext(OptionsContext)(defualtValue);
 }
-export function ContentContextual1() {
+export const ContentContextual1 = React.memo<any>(function ContentContextual1() {
   return <>
+    <div><i>rerendered counter: {counters.contextual1++}</i></div>
     <OptionsProvider key={'abc'}><ContentContextual2/></OptionsProvider>
   </>;
-}
-export function ContentContextual2() {
+});
+export const ContentContextual2 = React.memo<any>(function ContentContextual2() {
   const [options, setOptions] = useOptions({ x: 'y' });
   return <>
     <b>provide contextual stores</b>
+    <div><i>rerendered counter: {counters.contextual2++}</i></div>
     <div>{`const [options, setOptions] = useOptions({ x: 'y' });`}</div>
     <div>options == {JSON.stringify(options)}</div>
     <button onClick={() => setOptions({ x: 'z' })}>{`setOptions({ x: 'z' })`}</button>
     <button onClick={() => setOptions({ x: 'w' })}>{`setOptions({ x: 'w' })`}</button>
   </>;
-}
+});
 
-let rerendered = 0;
-export function Content() {
+export const Content = React.memo<any>(function Content() {
   return <>
-    <div>page rerendered: {rerendered++}</div>
+    <b>{pckg.name}@{pckg.version}</b>
+    <br/>
+    <a href={pckg.homepage}>{pckg.homepage}</a>
+    <br/>
+    <a href="https://github.com/deepcase/store/blob/example/pages/index.tsx">example sources</a>
     <hr/>
-    <div>store</div>
+    <div><i>rerendered counter: {counters.page++}</i></div>
     <hr/>
     <ContentQuery/>
+    <hr/>
     <ContentQueryListener/>
     <hr/>
     <ContentCookies/>
+    <hr/>
     <ContentCookiesListener/>
     <hr/>
     <ContentLocal/>
+    <hr/>
     <ContentLocalListener/>
     <hr/>
     <ContentCapacitor/>
+    <hr/>
     <ContentCapacitorListener/>
     <hr/>
     <ContentToken/>
     <hr/>
     <ContentContextual1/>
   </>;
-}
+});
 
-export const Providers = () => {
+export const Providers = React.memo<any>(function Providers() {
   return <QueryStoreProvider>
     <CookiesStoreProvider>
       <LocalStoreProvider>
@@ -167,7 +203,7 @@ export const Providers = () => {
       </LocalStoreProvider>
     </CookiesStoreProvider>
   </QueryStoreProvider>;
-};
+});
 
 export default function Index() {
   return (<div>
