027964c9fa8d379740a3ce2f1ff2a74342728de6
Ivan S Glazunov <ivansglazunov@gmail.com>
2021-02-18 00:27:47 +0000

    basePath for dev

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  basePath: '/store',
+  basePath: isProd ? '/store' : '',
   assetPrefix: isProd ? 'https://cdn.statically.io/gh/deepcase/store/gh-pages/' : '',
 };
