ce698cbfbd317d4c18391cd29a51bccebd31f844
FreePhoenix888 <freephoenix888@gmail.com>
2023-07-21 11:21:57 +0000

    Deploying to gh-pages from @ deep-foundation/store@c1dfbed2323d8e323743d059b00e1357b42ac719 🚀

--- _next/static/jqrCYQWpD6xgB-mMBzqwa/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F29107295.8bb6da8bd65140c2fc8c.js","static\u002Fchunks\u002Fpages\u002Findex-0db060da1ef36ba9e36c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e3a89cd3c1ac261815fc.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
